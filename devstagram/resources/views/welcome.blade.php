fde0313be4e8803123c33492789325daddcc5e3b
darwinalexisguerrerobaos <darwinalexisguerrerobaos@gmail.com>
2023-11-22 20:38:20 -0500

    diego

---
+++ devstagram/resources/views/welcome.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        @vite('resources/css/app.css')
+        <title>Devstagram</title>
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    </head>
+    <body class="antialiased">
+    <header class="w-100 h-20  bg-slate-400" >
+        <h1 class="text-red-500 py-7 px-5">DEVSTAGRAM</h1>
+        <p class="hover:font-bold">INICIAR SESION</p>
+
+    </header>
+    <div class="w-100 grid grid-cols-2  gap-9 px-12 relative top-16" >
+        <div class="col-start-1 col-end-7">
+            <h1 class="font-extrabold text-lg flex justify-center">Devstagram</h1>
+        </div>
+        <div class="w-50 py-4">
+            <img src="{{ asset ('imagenes/registrar___5365563673deb11___.jpg')}}" class="w-150 h-80  boder rounded-lg">
+        </div>
+        <div class="ml-16">
+            <label  class="text-xl">NOMBRE</label>
+            <br>
+            <input type="text" placeholder="INGRESE SU NOMBRE" class="border p-2 w-full mt-4 mb-4  rounded-lg ">
+            <br>
+            <label class="text-xl">CORREO</label>
+
+            <br>
+            <input type="email" id="email" name="email" placeholder="E-mail" class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror">
+            <br>
+            <label class="text-xl">CLAVE</label>
+            <br>
+            <input class="border p-2 w-full mt-4 mb-4  rounded-lg " name="clave" type="password" placeholder="INGRESE EL CLAVE">
+            @error('clave'){{$message}}@enderror
+            <br>
+            <input type="submit" class="w-full h-10 bg-cyan-700 cursor-pointer  rounded-lg ">
+        </div>
+    </div>
+
+    </body>
+</html>
